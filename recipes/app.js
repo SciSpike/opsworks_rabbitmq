94b431bb1bfba8625ac0887a682535fa7dc6f3cf
JK <jonathan.kamke@gmail.com>
2015-07-07 09:29:07 -0700

    use elb

--- recipes/app.js
+++ recipes/app.js
@@ -1,3 +1,10 @@
-node[:deploy].each do |application, deploy|
-  node.set[:deploy][application][:environment_variables]["AMQP_HOST"]=node[:opsworks][:layers][node['rabbitmq']['opsworks']['layer_name']][:instances].values.map{ |x| x[:private_ip] } * ","
+layer = node['rabbitmq']['opsworks']['layer_name']
+layerId = node[:opsworks][:layers][layer][:id]
+node[:opsworks][:stack]['elb-load-balancers'].each do |i,elb|
+  if elb[:layer_id] == layerId
+    Chef::Log.info("elb for #{layer} found  with dns: #{elb[:dns_name]}.")
+    node[:deploy].each do |application, deploy|
+      node.set[:deploy][application][:environment_variables]["AMQP_HOST"]=elb[:dns_name]
+    end
+  end
 end
